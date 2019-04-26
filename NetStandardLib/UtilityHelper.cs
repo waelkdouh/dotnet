2491cd055111c95c89d53177cbcc8edd4203c24c
Wael Kdouh <waelkdouh@gmail.com>
2019-04-26 15:49:13 -0400

    Add project files.

---
+++ NetStandardLib/UtilityHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetStandardLib
+{
+    public class UtilityHelper
+    {
+        public int Calculate(int num1,int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
